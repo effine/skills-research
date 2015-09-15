17b4c1126bcf6962dfa1e5a5774954cb91c9d894
effine <verphen@163.com>
2015-09-15 10:54:59 +0800

    skills-research

---
+++ src/main/java/cn/effine/rabbitmq/Test.java
@@ -0,0 +1,15 @@
+
+/**
+ * @author effine
+ * @Date 2015年9月15日  上午10:53:24
+ * @email verphen#gmail.com
+ * @site http://www.effine.cn
+ */
+
+package cn.effine.rabbitmq;
+
+public class Test {
+
+}
+
+
